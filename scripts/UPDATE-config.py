eb673ddba05f8eac32e107744aaddcdce71e5491
Rich Horace <richhorace@gmail.com>
2020-07-07 17:02:21 -0700

    Updated browser_history.py to use pathlib library which elimates need for UPDATE-config.py

--- scripts/UPDATE-config.py
+++
@@ -1,4 +0,0 @@
-'''
-Include trailing / like this: USER_PATH = "/Users/richhorace/" 
-'''
-USER_PATH = "ADD_USER_PATH"
--- scripts/browser_history.py
+++ scripts/browser_history.py
@@ -3,13 +3,14 @@ import json
 import os
 import sqlite3
 import time
-
-from config import USER_PATH
 from local import LOG_DIR
+from pathlib import Path
+
+USER_PATH = str(Path.home())
 
 def get_path(browser,browser_params):
     if browser == 'brave':
-        BRAVE_PATH = "Library/Application Support/BraveSoftware/Brave-Browser/Default/History"
+        BRAVE_PATH = "/Library/Application Support/BraveSoftware/Brave-Browser/Default/History"
         hist_path = USER_PATH + BRAVE_PATH
         hist_sql = "SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime') AS visit_date, url, title AS url_title  FROM urls ORDER BY visit_date DESC"
         browser_params['browser_name'] = browser
@@ -17,7 +18,7 @@ def get_path(browser,browser_params):
         browser_params['hist_sql'] = hist_sql
         return browser_params
     elif browser == 'chrome':
-        CHROME_PATH = "Library/Application Support/Google/Chrome/Default/History"
+        CHROME_PATH = "/Library/Application Support/Google/Chrome/Default/History"
         hist_path = USER_PATH + CHROME_PATH
         hist_sql = "SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime') AS visit_date, url, title AS url_title  FROM urls ORDER BY visit_date DESC"
         browser_params['browser_name'] = browser
@@ -25,7 +26,7 @@ def get_path(browser,browser_params):
         browser_params['hist_sql'] = hist_sql
         return browser_params
     elif browser == 'edge':
-        EDGE_PATH = "Library/Application Support/Microsoft Edge/Default/History"
+        EDGE_PATH = "/Library/Application Support/Microsoft Edge/Default/History"
         hist_path = USER_PATH + EDGE_PATH
         hist_sql = "SELECT datetime(last_visit_time/1000000-11644473600,'unixepoch','localtime') AS visit_date, url, title AS url_title  FROM urls ORDER BY visit_date DESC"
         browser_params['browser_name'] = browser
@@ -33,7 +34,7 @@ def get_path(browser,browser_params):
         browser_params['hist_sql'] = hist_sql
         return browser_params
     elif browser == 'firefox':
-        FIREFOX_PATH = "Library/Application Support/Firefox/Profiles/tsmorvto.default-release/places.sqlite"
+        FIREFOX_PATH = "/Library/Application Support/Firefox/Profiles/tsmorvto.default-release/places.sqlite"
         hist_path = USER_PATH + FIREFOX_PATH
         hist_sql = "SELECT datetime(moz_historyvisits.visit_date/1000000,'unixepoch','localtime') AS visit_date, moz_places.url AS url, moz_places.title AS url_title FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id ORDER BY visit_date DESC"
         browser_params['browser_name'] = browser
@@ -41,7 +42,7 @@ def get_path(browser,browser_params):
         browser_params['hist_sql'] = hist_sql
         return browser_params
     elif browser == 'safari':
-        SAFARI_PATH = "Library/Safari/History.db"
+        SAFARI_PATH = "/Library/Safari/History.db"
         hist_path = USER_PATH + SAFARI_PATH
         hist_sql = "SELECT datetime(visit_time + 978307200, 'unixepoch', 'localtime') AS visit_date, url, title AS url_title FROM history_visits INNER JOIN history_items ON history_items.id = history_visits.history_item ORDER BY visit_date DESC"
         browser_params['browser_name'] = browser
